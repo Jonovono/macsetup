539bc9b92d8f69ce30315b00d4116a573e35f5e3
Jordan Howlett <jordan@howlett.io>
2017-07-26 22:57:55 -0400

    fix

--- .dots.sh
+++ .dots.sh
@@ -7,9 +7,9 @@ function runDots() {
     # Keep-alive: update existing `sudo` time stamp until the script has finished
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-    ./brew.sh 
-    ./aliases.sh
-    ./web.sh
+    # ./brew.sh 
+    # ./aliases.sh
+    # ./web.sh
     ./osxprep.sh
     ./macos.sh 
     ./app.sh
