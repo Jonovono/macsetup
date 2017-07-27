78892ece8d58aae4814663cfa06e20cdce59a48a
Jordan Howlett <jordan@howlett.io>
2017-07-26 22:40:07 -0400

    Add some stuff

--- app.sh
+++ app.sh
@@ -37,7 +37,7 @@ brew cask install --appdir="/Applications" dayone
 brew cask install --appdir="/Applications" rescuetime
 brew cask install --appdir="/Applications" fantastical
 brew cask install --appdir="/Applications" backblaze
-# brew cask install --appdir="/Applications" flux
+brew cask install --appdir="/Applications" flux
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" hyper
 
